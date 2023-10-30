69a7877930eba5a01a85ea326445f8153564597a
Stefan Huckschlag <stefan.huckschlag@web.de>
2023-10-30 00:16:49 +0000

    feat(_index): check login state and redirect

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,7 @@
-import type { MetaFunction } from "@remix-run/node";
+import { redirect, type LoaderFunction } from "@remix-run/node";
+import { requireUserId } from "~/utils/auth.server";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request);
+  return redirect("/home");
 };
-
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-}
